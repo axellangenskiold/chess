3fd65f73a20e6bca4a12a7df4028eae660ada873
axel.langenskiold.2531 <ax2531la-s@student.lu.se>
2024-01-06 15:40:22 +0100

    added method getMove() and another constructor in Pos

--- app/src/main/java/chess/Pos.java
+++ app/src/main/java/chess/Pos.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.pieces.Move;
+
 public class Pos {
 
     Object[] pos = {null, null};
@@ -9,11 +11,20 @@ public class Pos {
         pos[1] = col;
     }
 
-    public char getRow() {
-        return (char) pos[0];
+    public Pos(int row, int col) {
+        this((char) (row + 97), col);
+    }
+
+    public int getRow() {
+        return (int) pos[0] - 97; //convert char to number (a returns 0, b returns 2)
     }
 
     public int getCol() {
         return (int) pos[1];
+    }
+
+    public static Move getMove(Pos pos, int x, int y) {
+        
+        return new Move(pos, new Pos(pos.getRow() - x, pos.getCol() - y));
     }
 }
